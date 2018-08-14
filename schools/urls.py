4cfc11ee071138da9715cc6361d1404282f41a64
angelanayiga <angelanayiga@gmail.com>
2018-08-14 11:38:05 +0300

    CRUD Views

--- schools/urls.py
+++ schools/urls.py
@@ -6,4 +6,7 @@ app_name = 'schools'
 urlpatterns = [
     url(r'^$', views.SchoolListView.as_view(), name='school_list'),
     url(r'^(?P<pk>\d+)/$', views.SchoolDetailView.as_view(), name='school_details'),
+    url(r'^update/(?P<pk>\d+)/$', views.SchoolUpdateView.as_view(), name='update'),
+    url(r'^delete/(?P<pk>\d+)/$', views.SchoolDeleteView.as_view(), name='delete'),
+    url(r'^create/$', views.SchoolCreateView.as_view(), name='create'),
 ]
--- schools/views.py
+++ schools/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, DetailView, View
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from schools.models import School, Student
+from django.urls import reverse_lazy
 
 
 # Create your views here.
@@ -15,3 +16,18 @@ class SchoolListView(ListView):  # default context name is <model_name>_list
 class SchoolDetailView(DetailView):  # default context name is <model_name>
     model = School
     template_name = 'schools/school_details.html'
+
+
+class SchoolCreateView(CreateView):
+    fields = ('name', 'principal', 'motto')
+    model = School
+
+
+class SchoolUpdateView(UpdateView):
+    fields = ('principal',)
+    model = School
+
+
+class SchoolDeleteView(DeleteView):
+    model = School
+    success_url = reverse_lazy('schools:school_list')
